fee6ea56321591b018bcf434959b9018f2d3afce
Brian Schwinn <bschwinn@users.noreply.github.com>
2026-05-02 19:50:25 -0400

    mobile nav love, improve scrolling (#9)

--- ui/src/components/layout/layout.tsx
+++ ui/src/components/layout/layout.tsx
@@ -2,23 +2,57 @@ import { type ReactNode } from "react";
 
 import Box from "@mui/material/Box";
 
+import { Navigation } from "./nav";
+import {
+  PageHeader,
+  type PageHeaderProps,
+} from "@/components/layout/page-header";
 import { MobileHeader } from "./mobile-header";
 import { MobileFooter } from "./mobile-footer";
-import { Navigation } from "./nav";
 
-export const Layout = ({ children }: { children: ReactNode }) => {
+const mobileHeaderHeight = 80;
+const mobileFooterHeight = 88;
+
+export type LayoutProps = {
+  children: ReactNode;
+  showHeader?: boolean;
+} & PageHeaderProps;
+
+export const Layout = ({
+  title,
+  subtitle,
+  action,
+  children,
+  showHeader = true,
+}: LayoutProps) => {
   return (
     <Box
       sx={{
         display: "flex",
         flexDirection: "column",
-        minHeight: "calc( 100vh )",
+        height: "100vh",
       }}
     >
-      <Box sx={{ display: { xs: "block", md: "none" } }}>
+      <Box
+        sx={{
+          display: { xs: "block", md: "none" },
+          boxShadow: "0 16px 16px -16px #000",
+          zIndex: 40,
+        }}
+      >
         <MobileHeader />
       </Box>
-      <Box sx={{ flexGrow: 1, display: "flex", flexDirection: "row" }}>
+      <Box
+        sx={{
+          flexGrow: 1,
+          display: "flex",
+          flexDirection: "row",
+          height: {
+            xs: `calc( 100vh - ${mobileHeaderHeight + mobileFooterHeight}px)`,
+            md: "100vh",
+          },
+        }}
+      >
         <Box
           component="aside"
           sx={(th) => ({
@@ -32,13 +66,31 @@ export const Layout = ({ children }: { children: ReactNode }) => {
         >
           <Navigation />
         </Box>
-        <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-          <Box sx={{ maxWidth: '1024px', mx: 'auto' }}>
-            {children}
-          </Box>
+        <Box
+          component="main"
+          sx={{
+            flexGrow: 1,
+            p: { xs: 2, md: 3 },
+            height: {
+              xs: `calc( 100vh - ${mobileHeaderHeight + mobileFooterHeight}px)`,
+              md: "100vh",
+            },
+            overflowY: "auto",
+          }}
+        >
+          {showHeader && (
+            <PageHeader title={title} subtitle={subtitle} action={action} />
+          )}
+          <Box sx={{ maxWidth: "1024px", mx: "auto" }}>{children}</Box>
         </Box>
       </Box>
-      <Box sx={{ display: { xs: "block", md: "none" } }}>
+      <Box
+        sx={{
+          display: { xs: "block", md: "none" },
+          boxShadow: "0 -16px 16px -16px #000",
+          zIndex: 40,
+        }}
+      >
         <MobileFooter />
       </Box>
     </Box>
